c430626baafc0f302a3231823a596735085dac57
Evgeniy-Nazarov <perfectyam@gmail.com>
2023-02-16 13:08:22 -0500

    brrrr

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
-const refs = {
-    input: document.querySelector('#name-input'),
-    output: document.querySelector('#name-output'),
-};
+const inputEl = document.getElementById(`search-box`);
+const countryListEl = document.querySelector(`.country-list`);
+const countryInfoEl = document.querySelector(`.country-info`);
