ad840817732679481f9f8eaf6cab1df5c43388c8
Evgeniy-Nazarov <perfectyam@gmail.com>
2023-02-16 11:52:32 -0500

    111111

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#name-input'),
+    output: document.querySelector('#name-output'),
+};
